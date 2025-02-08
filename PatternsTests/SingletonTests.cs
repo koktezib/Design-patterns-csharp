2464982f5f101fa082c4ae32c43a39fab8c98848
koktezib <91209347+koktezib@users.noreply.github.com>
2025-02-08 13:30:09 +0300

    Add UnitTests project aka PatternsTests Add SingletonTests

---
+++ PatternsTests/SingletonTests.cs
@@ -0,0 +1,38 @@
+using Patterns.Patterns.Singleton;
+
+namespace PatternsTests;
+
+[TestFixture]
+public class SingletonTests
+{
+    [Test]
+    public void Instance_ShouldNotBeNull()
+    {
+        // Act
+        var instance = Singleton.Instance;
+        
+        // Assert
+        Assert.IsNotNull(instance);
+    }
+
+    [Test]
+    public void Instance_ShouldReturnSameObjectOnMultipleCalls()
+    {
+        // Act
+        var instance1 = Singleton.Instance;
+        var instance2 = Singleton.Instance;
+        
+        // Assert
+        Assert.That(instance2, Is.SameAs(instance1), "Оба вызова должны вернуть один и тот же экземпляр");
+    }
+
+    [Test]
+    public void Singleton_DefaultName_ShouldBeRider()
+    {
+        // Act
+        var instance = Singleton.Instance;
+        
+        // Assert
+        Assert.That(instance.Name, Is.EqualTo("Rider"), "Значение Name должно быть 'Rider'");
+    }
+}
